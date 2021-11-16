db710dae0d85d404287c5dc4b3f1dcbaa63ace76
kayeff78 <f.kizeyi@gmail.com>
2021-11-16 09:31:30 +0100

    changement de logo

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
       
   <div class="container-fluid">
     <div class="container">
-    <img src="img/ferrari.jpg " alt="..." class="logo">
+    <img src="img/ferrari.jpg " alt="..." class="logo">la ferrari c'est beau gosse
     <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
